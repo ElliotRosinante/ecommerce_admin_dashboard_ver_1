4ee7751950bc16636abfd44ff8664b138635b7fb
Elliot Lassey <lasseyelliot86@gmail.com>
2023-11-24 20:56:55 +0000

    created and added components and routes for the admin dashboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("1d307490-8efd-4cde-94d3-2fbad872607d");
 
   return (
     <Container>
